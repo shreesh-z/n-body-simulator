7109d0baa7275b8f0b5061e74f010d8dadc644b6
Shreesh Kulkarni <33529229+shreesh-z@users.noreply.github.com>
2020-07-28 21:21:39 +0530

    Simulator source file
    The executable can evolve the bodies in time using the RK4 technique. In the next commit, a real time openGL based graphical rendering of the system is planned.

--- barnes_hut.cpp
+++ barnes_hut.cpp
@@ -2,8 +2,45 @@
 #include <random>
 #include <cmath>
 
-//for value of gravitation constant
-#define G 0.0000000000005
+/*
+	------------------------GLOBAL VARIABLES---------------------------
+*/
+
+//number of bodies in the space. Low number during development.
+int N = 10;
+//Gravitation constant, can be varied
+double G = 0.0000000005;
+//theta criterion (inverse)
+double thetaCritInv = 2.0;
+//half dimensions of the square space
+double squareDims = 1000.0;
+//bounds of the space in cartesian coordinates
+double leftPos = -squareDims, downPos = -squareDims, halfSide = squareDims;
+
+/*
+	---------------------------STRUCT: BODY-----------------------------
+	
+	Attributes:
+		ID     : 0 for interior node bodies (COM bodies) and positive int for real bodies
+		mass   : mass of the body
+		coords : x,y position and vx,vy velocity of the body
+		force  : x,y gravitational force acting on the body
+		
+	Constructors:
+		Body(ID, m_, posX, posY, velX, velY)
+			for a general body
+		Body(ID, m_, posX, posY)
+			for a body starting from rest
+		Body()
+			for an interior node in the B-H Tree
+	
+	Member Functions:
+		updateCOM(Body*):
+			For bodies of interior nodes with ID = 0. When a new body is added to the subtree
+			of the interior node, this function is called on the COM body of the node.
+		initForce():
+			Initializes the force acting on the body to 0.
+*/
 
 struct Body{
 	/*ID is used while calculating forces
@@ -11,8 +48,12 @@ struct Body{
 	the external node, force is zero*/
 	int ID;
 	double mass;
-	double coords[4];
+	
 	//ordering is posX, posY, velX, velY;
+	double coords[4];
+	
+	//Force acting on the body
+	double force[2];
 
 	//For a general body
 	Body(int ID_, double m_, double posX_, double posY_, double velX_, double velY_){
@@ -22,7 +63,9 @@ struct Body{
 		coords[1] = posY_;
 		coords[2] = velX_;
 		coords[3] = velY_;
+		force[0] = force[1] = 0.0;
 	}
+	
 	//For a body starting from rest
 	Body(int ID_, double m_, double posX_, double posY_) 
 	: Body(ID_, m_, posX_, posY_, 0.0, 0.0){}
@@ -36,28 +79,51 @@ struct Body{
 		//Other body's attributes remain unchanged
 		double totMass = mass + otherBod->mass;
 		for( int i = 0; i < 4; i++ ){
-			coords[i] = (coords[i]*mass + otherBod->coords[i]*otherBod->mass )/totMass;
+			coords[i] = ( coords[i]*mass + otherBod->coords[i]*otherBod->mass )/totMass;
 		}
 		mass = totMass;
 	}
+	
+	//initializes force on body to zero
+	void initForce(){
+		force[0] = force[1] = 0.0;
+	}
 };
+
+
+
+/*
+	-------------------------STRUCT: BARNES-HUT TREE NODE--------------------------------
+	
+	Attributes:
+		*body      : pointer to the body contained in the node
+		left, down : coordinates of the bottom-left corner of the square space represented by the node
+		s          : half side length of the square represented by the node
+		*quadNodes : pointer array that houses the 4 sub trees of the node
+		
+	Constructors:
+		Node(*body, left_, down_, side)
+			for a general node with a general body contained in it
+		Node(left_, down_, side)
+			for an internal node that starts with having at rest with no mass
+*/
 
 struct Node{
 	//Either the body inside the node or the COM body
 	Body *body;
 	//The left and down coords of the quad tree cell
 	double left, down;
-	//The half of side length of the cell
+	//The HALF of side length of the cell (NOTE : HALF OF SIDE LENGTH)
 	double s;
 	
 	//The four nodes inside the given node
 	//Since four nodes, a quad tree is formed
+	//node ordering: NE, SE, NW, SW
 	Node* quadNodes[4];
-	//node ordering: SE, NE, SW, NW
 	
-	//Constructor for a general body
-	Node(Body *bod_, double left_, double down_, double side){
-		body = bod_;
+	//Constructor for a general node
+	Node(Body *body_, double left_, double down_, double side){
+		body = body_;
 		s = side;
 		left = left_;
 		down = down_;
@@ -70,26 +136,51 @@ struct Node{
 	: Node(new Body(0, 0.0, 0.0, 0.0), left_, down_, side){}
 };
 
+/*
+	---------------------------------FUNCTIONS----------------------------------
+	
+	insert:
+		Inserts a body into the BH Tree recursively
+	printTree:
+		prints the BH Tree recursively (for debugging only)
+	findForce:
+		finds the force on a body due to all the other bodies inside the BHTree
+	deleteTree:
+		deletes the entire BHTree to start fresh in the next iteration
+	RK4:
+		RK4 integrator of the whole system (4xN dimension ODE solver)
+	init:
+		initializes the Bodies array
+	evolve:
+		evolves the bodies in time
+	main:
+		main function for running the program on the console
+	
+*/
+
 //Inserts the body inBody into the B-H quad tree recursively
 void insert(Node *root, Body *inBody){
 	if( root->body->ID == 0 ){
 		//If the root node is an interior node
 		
-		//The index of list of quad nodes
+		//The index of one of four nodes in which the newly added body is located
+		//0: NE, 1:SE, 2:NW, 3:SW
 		int nodeListInd = 0;
-		//To store offsets if a new node has to be created
+		
+		//To store offsets away from the current node's (left, down)
+		//where a new interior node will be created
 		int directionX = 0, directionY = 0;
 		
-		//First update COM of combined system
+		//Updating COM of combined system
 		root->body->updateCOM(inBody);
 		
-		//now figure out which quad node to place body in
+		//now figure out which direction quad node to place body in
 		if( inBody->coords[0] >= (root->left+root->s) ){
 			//If body is in East
 			if( inBody->coords[1] >= (root->down+root->s) ){
 				//If body is in NE
 				nodeListInd = 0;
-				//Take steps in both direction
+				//Take steps in both directions
 				directionX = 1;
 				directionY = 1;
 			}else{
@@ -114,9 +205,9 @@ void insert(Node *root, Body *inBody){
 		if( root->quadNodes[nodeListInd] == NULL ){
 			//Make new node if null, then exit
 			root->quadNodes[nodeListInd] = new Node(inBody, 
-													root->left + directionX * root->s, 
-													root->down + directionY * root->s, 
-													root->s/2);
+									root->left + directionX * root->s, 
+									root->down + directionY * root->s, 
+									root->s/2);
 			return;
 		}else{
 			//If a node exists there already, add recursively
@@ -126,19 +217,17 @@ void insert(Node *root, Body *inBody){
 	}else{
 		//If root node is an exterior node
 		
-		//Making a new interior COM node to replace the exterior node
+		//Making a new interior COM body to replace the exterior node
 		Body *newBod = new Body();
-		Body *rootBod = root->body;
 		
-		//Adding the ext node body and new inBody to the new interior node
-		newBod->updateCOM(rootBod);
-		newBod->updateCOM(inBody);
+		//storing the address of the old exterior node's body
+		Body *oldBod = root->body;
 		
-		//Setting the body of the node to an interior node
+		//Setting the body of the node to the interior node-body
 		root->body = newBod;
 		
 		//Recursively add the two bodies into the root node again
-		insert(root, rootBod);
+		insert(root, oldBod);
 		insert(root, inBody);
 		return;
 	}
@@ -154,94 +243,173 @@ void printTree(Node *root){
 	std::cout << root->body->ID << ' ' << root->body->mass << ' ' << root->s << "\n";
 }
 
-//Used to find steps during RK4, unused now
-/*
-double* step(double *functs, double *coords, double timestep){
-	double newVect[4];
-	for( int i = 0; i < 4; i++ ){
-		newVect[i] = coords[i] + functs[i]*timestep;
-	}
-	return newVect;
-}*/
-
-//returns the net force on body due to the bodies present inside the B-H tree
+//find net force on a body due to the bodies present inside the B-H tree
 //acts recursively
-//call this function with an initial force {0.0, 0.0}
-double* findForce(double* force, Body *body, Node *root, double thetaCritInv){
+void findForce(Body *body, Node *root, double coeffCap){
 	
 	//If the same body is encountered in the tree, don't add anything to the force
 	if( body->ID == root->body->ID )
-		return force;
+		return;
 	
 	//X and Y distances between the two bodies
-	double diffX = body->coords[0] - root->body->coords[0], diffY = body->coords[1] - root->body->coords[1];
+	double diffX = root->body->coords[0] - body->coords[0];
+	double diffY = root->body->coords[1] - body->coords[1];
 	
 	//Net distance between two bodies
 	double dist = std::hypot(diffX, diffY);
 	
-	if( dist > thetaCritInv*root->s*2 ){
+	if( dist > thetaCritInv*root->s*2 || root->body->ID != 0){
 		//If COM body satisfies theta criterion
+		//OR if an exterior node has been reached
 		double coeff = (G * body->mass * root->body->mass)/std::pow(dist,3);
-		force[0] += coeff*diffX;
-		force[1] += coeff*diffY;
-		return force;
+		
+		//capping coeff value so infinities are not produced
+		coeff = coeff>coeffCap ? coeffCap : coeff;
+		
+		//calculating force acc. to Newton's Law of Gravitation
+		body->force[0] += coeff*diffX;
+		body->force[1] += coeff*diffY;
+		return;
 	}
 	else{
+		//COM body doesn't satisfy theta criterion, go deeper into the tree
 		//iterating over all the 4 nodes inside current node
 		for( int i = 0; i < 4; i++ ){
 			if( root->quadNodes[i] != NULL ){
-				//finding the force on current body by bodies inside current node recursively
-				double *newForce = new double[2];
-				newForce = findForce(force, body, root->quadNodes[i], thetaCritInv);
-				force[0] += newForce[0];
-				force[1] += newForce[1];
+				//recursively finding the force on current body by bodies inside current node
+				findForce(body, root->quadNodes[i], coeffCap);
 			}
 		}
-		return force;
+		return;
 	}
 }
- 
 
-int main(){
-	//Total number of bodies
-	int N = 10;
+//to delete the tree once all forces have been found
+void deleteTree(Node *node){
+	if( node == NULL )
+		return;
+	for( int i = 0; i < 4; i++ )
+		deleteTree(node->quadNodes[i]);
 	
-	//half dimensions of the square space
-	double dims = 1000.0;
-	double leftPos = -dims, downPos = -dims, halfSide = dims;
-	int thetaCritInv = 2;	//In this case, theta criterion is 0.5, ie d > 2*sidelength
+	delete node;
+}
+
+//RK4 integrator
+void RK4(int N, Body **Bodies, double timestep, double coeffCap){
 	
-	//Root node of the barnes hut quad tree
-	Node* root = new Node(leftPos, downPos, halfSide);
+	//Coords of bodies at time t_init
+	double OrigCoords[4][N];
+	for( int i = 0; i < N; i++){
+		for( int j = 0; j < 4; j++){
+			OrigCoords[j][i] = Bodies[i]->coords[j];
+		}
+	}
 	
-	//array holding all the bodies
-	Body* Bodies[N];
+	//stores the 4 RK4 function values
+	double functs[4][N][4];
 	
+	for( int rk = 0; rk < 4; rk++ ){
+		
+		//The root node of the BH tree, used to find the forces
+		Node *root = new Node(leftPos, downPos, halfSide);
+		
+		//First add all bodies to the BH Tree
+		for( int i = 0; i < N; i++ ){
+			//Initialize forces with 0
+			Bodies[i]->initForce();
+			insert(root, Bodies[i]);
+		}
+		
+		//Then find force acting on each body
+		for( int i = 0; i < N; i++ ){
+			findForce(Bodies[i], root, coeffCap);
+		}
+		
+		//Now delete the tree for a fresh tree in the next RK4 iteration
+		deleteTree(root);
+		
+		//storing function values found at this RK step (ie. velocity and force)
+		for( int i = 0; i < N; i++ ){
+			for( int j = 0; j < 2; j++ ){
+				//storing velocity values as functs for displacement
+				functs[j][i][rk] = Bodies[i]->coords[j+2];
+				//storing force values as functs for velocity
+				functs[j+2][i][rk] = Bodies[i]->force[j];
+			}
+		}
+		
+		//Now forces on all bodies have been found. RK4 Step is to be taken.
+		
+		if( rk < 2 ){
+			for( int i = 0; i < N; i++ ){
+				for( int j = 0; j < 2; j++ ){
+					//First x and y coords
+					Bodies[i]->coords[j] = OrigCoords[j][i] + (timestep/2)*Bodies[i]->coords[j+2];
+					//Now vx and vy
+					Bodies[i]->coords[j+2] = OrigCoords[j+2][i] + (timestep/2)*Bodies[i]->force[j];
+				}
+			}
+		}else if( rk == 2 ){
+			for( int i = 0; i < N; i++ ){
+				for( int j = 0; j < 2; j++ ){
+					//First x and y coords
+					Bodies[i]->coords[j] = OrigCoords[j][i] + timestep*Bodies[i]->coords[j+2];
+					//Now vx and vy
+					Bodies[i]->coords[j+2] = OrigCoords[j+2][i] + timestep*Bodies[i]->force[j];
+				}
+			}
+		}else{
+			//final RK4 step
+			for( int i = 0; i < N; i++ ){
+				for( int j = 0; j < 4; j++ ){
+					//taking full RK4 step with the 4xNx4 matrix
+					Bodies[i]->coords[j] = OrigCoords[j][i] + (timestep/6)*(
+												functs[j][i][0] +
+												2*( functs[j][i][1] + functs[j][i][2] ) +
+												functs[j][i][3]
+												);
+				}
+			}
+		}
+	}
+}
+
+//Initializing the bodies array
+void init(Body **Bodies){
 	//pRNG functions from stdlib
 	std::mt19937 gen(1);		//constant seed during development
-	std::uniform_real_distribution<> disMass(1.0, 2.0), disPos(leftPos,-leftPos);		
+	std::uniform_real_distribution<> disMass(1.0, 2.0), disPos(leftPos/2,-leftPos/2);		
 	
 	//filling the bodies array with random masses and initial positions
 	for(int i = 0; i < N; i++){
 		//Bodies are ID'd starting from 1
 		Bodies[i] = new Body(i+1, disMass(gen), disPos(gen), disPos(gen));
 	}
+}
+
+//evolving the bodies in time
+void evolve(Body **Bodies){
+	double maxTime = 5;
+	double timestep = 1;
+	int count = (int)(maxTime/timestep);
 	
-	//Adding bodies to the BH tree
-	for (int i = 0; i < N; i++){
-		insert(root, Bodies[i]);
+	for( int t = 0; t < count; t++ ){
+		RK4(N, Bodies, timestep, 10000);
+		for( int i = 0; i < N; i++){
+			//Outputting the position and velocity values of each body
+			std::cout << Bodies[i]->coords[0] << ' ' << Bodies[i]->coords[1] << ' ' << Bodies[i]->coords[2] << ' ' << Bodies[i]->coords[2] << "\n";
+		}
+		std::cout << "\n";
 	}
+}
+
+int main(){
 	
-	//std::cout << "Tree traversal:\n";
-	//printTree(root);
-	//std::cout << "\n";
+	//array holding all the bodies
+	Body *Bodies[N];
 	
-	std::cout << "Forces on bodies:\n";
-	for( int i = 0; i < N; i++ ){
-		double *force = new double[2];
-		force = findForce(force, Bodies[i], root, thetaCritInv);
-		std::cout << force[0] << ' ' << force[1] << "\n";
-	}
+	init(Bodies);
+	evolve(Bodies);
 	
-	return 0;
+	return 0;	
 }
